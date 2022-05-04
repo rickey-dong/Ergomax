1748867a5f7de2dccfce3d7cfffe8a338f32d0c8
Rickey Dong <rickeyd2004@gmail.com>
2022-05-04 15:50:51 -0400

    added constants of keypoints

--- app.py
+++ app.py
@@ -100,6 +100,24 @@ def feature_extraction(image_file):
 
     return cleaner_2d_array
 
+NOSE = 0
+LEFT_EYE = 1
+RIGHT_EYE = 2
+LEFT_EAR = 3
+RIGHT_EAR = 4
+LEFT_SHOULDER = 5
+RIGHT_SHOULDER = 6
+LEFT_ELBOW = 7
+RIGHT_ELBOW = 8
+LEFT_WRIST = 9
+RIGHT_WRIST = 10
+LEFT_HIP = 11
+RIGHT_HIP = 12
+LEFT_KNEE = 13
+RIGHT_KNEE = 14
+LEFT_ANKLE = 15
+RIGHT_ANKLE = 16
+
 def has_bad_posture(ideal, current):
     """
     returns true if user has bad posture currently,
@@ -107,4 +125,5 @@ def has_bad_posture(ideal, current):
     """
     # MATH
     # A LOT OF MATH
+    # https://pyimagesearch.com/2015/01/19/find-distance-camera-objectmarker-using-python-opencv/
     pass
